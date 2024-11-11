98a9f4f53bb3dc8cd000a94c01b812e92cea32c9
Yota0525 <167695062+Yota0525@users.noreply.github.com>
2024-11-11 17:34:41 +0900

    7章まで完了

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -3,15 +3,23 @@ import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 type Props = {
     params: {
         slug: string;
     };
+
+    SearchParams: {
+        dk?: string;
+    };
 };
 
-export default async function Page({ params }: Props) {
-    const data = await getNewsDetail(params.slug).catch(notFound);
+
+export default async function Page({ params, searchParams }: Props) {
+    const data = await getNewsDetail(params.slug,{
+        draftKey:searchParams.dk,
+    }).catch(notFound);
 
     return (
         <>
